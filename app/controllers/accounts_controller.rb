205d4284423ab3b586620823ec15a20df945254c
Jordan Rand <jorrand@gmail.com>
2018-09-07 14:20:57 -0600

    flash message

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,13 +1,53 @@
 class AccountsController < ApplicationController
+  
+  before_action :set_account, only: [:show, :edit, :update]
+
   def index
+    @accounts = current_user.accounts
   end
 
   def show
   end
 
   def new
+    @account = Account.new
+  end
+
+  def create
+    @account = current_user.accounts.new(account_params)
+
+    if @account.save
+      flash[:success] = 'Account Created'
+      redirect_to accounts_path
+    else
+      flash[:error] = "Error: #{@account.errors.full_messages.join("\n")}"
+      render :new
+    end
+  end
+
+  def update
+    if @account.update(account_params)
+      redirect_to accounts_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @account.destroy
   end
 
   def edit
+
   end
+
+  private
+
+    def set_account
+      @account = current_user.accounts.find(params[:id])
+    end
+
+    def account_params
+      params.require(:account).permit(:name, :balance)
+    end
 end
